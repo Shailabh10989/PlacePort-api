2613f587c33ec8833d858f1fd073cd1a31a4cd1a
Shailabh <shailabh10989@gmail.com>
2024-03-26 02:11:16 +0530

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // unique: true is just to increase the query speed
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
+});
+
+userSchema.plugin(uniqueValidator); // uniqueValidator will allow to create new user only if email id doesn't already exist
+
+module.exports = mongoose.model("User", userSchema);
---
+++ util/location.js
@@ -0,0 +1,8 @@
+async function getCoordsForAddress(address) {
+  return {
+    lat: 12.9507782,
+    lng: 77.6967257,
+  };
+}
+
+module.exports = getCoordsForAddress;
